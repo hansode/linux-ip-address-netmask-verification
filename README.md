4b9e18d23337e7d7ea34a5d4bb842a23c5fb705f
Masahito Yoshida <masahito@axsh.net>
2014-03-15 11:39:17 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+Linux: IP address netmask
+=========================
+
+System Requirements
+-------------------
+
++ CentOS 6.4
+
+Getting Started
+---------------
+
+```
+$ vagrant up
+```
---
+++ prefix.24-32/config.d/node01.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+function append_networking_param() {
+  local ifname=${1:-eth0}
+  shift; eval local ${@}
+
+  cat <<-EOS | tee -a /etc/sysconfig/network-scripts/ifcfg-${ifname}
+	IPADDR=${ip}
+	NETMASK=${mask}
+	EOS
+}
+
+node=node01
+#node=node02
+
+case "${node}" in
+  node01) ip4=18 opposit_ip4=19 ;;
+  node02) ip4=19 opposit_ip4=18 ;;
+esac
+
+mkdir -p /etc/vipple/vip-{up,down}.d
+
+append_networking_param eth1 ip=10.126.5.${ip4} mask=255.255.255.0
+
+##
+
+service network restart
