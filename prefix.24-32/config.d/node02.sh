eddf8c390485028d94c7397670da9b060d7e5604
Masahito Yoshida <masahito@axsh.net>
2014-03-17 12:14:04 +0900

    add virtual ip address to eth1.

--- prefix.24-32/config.d/node02.sh
+++ prefix.24-32/config.d/node02.sh
@@ -21,8 +21,8 @@ function append_networking_param() {
 node=node02
 
 case "${node}" in
-  node01) ip4=18 partner_ip4=19 ;;
-  node02) ip4=19 partner_ip4=18 ;;
+  node01) ip4=18 partner_ip4=19 virtual_ip4=17 ;;
+  node02) ip4=19 partner_ip4=18 virtual_ip4=17 ;;
 esac
 
 mkdir -p /etc/vipple/vip-{up,down}.d
@@ -32,3 +32,17 @@ append_networking_param eth1 ip=10.126.5.${ip4} mask=255.255.255.0
 ##
 
 service network restart
+
+##
+
+prefix_len=32
+
+case "${node}" in
+  node01)
+    /sbin/ip addr add 10.126.5.${virtual_ip4}/${prefix_len} dev eth1
+    ;;
+  node02)
+    ping -c 1 -W 3 10.126.5.${partner_ip4}
+    ping -c 1 -W 3 10.126.5.${virtual_ip4} || :
+    ;;
+esac
